81aebf9d165159304831a23a6ee0702bc6c8812a
Ronny <mwithiamoki@gmail.com>
2020-10-30 01:07:50 +0300

    added test project on test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Project, Profile, User
+
+new_user = User(username='kaphie')
+
+test_project = Project(
+    id=1,
+    title='Test Hello',
+    details='a test hello world app',
+    link='https://github.com/testhello.git',
+    user=new_user, image='/media/test_hello.jpg',
+    user_project_id='1',
+    design='7',
+    usability='8',
+    creativity='9',
+    content='8',
+    vote_submissions='87'
+)
