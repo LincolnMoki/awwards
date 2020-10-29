0959651906a79e557f1c331c54caeab29680f4ae
Ronny <mwithiamoki@gmail.com>
2020-10-30 00:47:54 +0300

    defined signup on views

--- app/views.py
+++ app/views.py
@@ -23,3 +23,21 @@ def index(request):
     except Project.DoesNotExist:
         posts = None
     return render(request, 'main/index.html', {'posts': posts, 'form': form, 'profile':profile})
+
+def signup(request):
+    global register_form
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignupForm()
+        register_form = {
+            'form': form,
+        }
+    return render(request, 'registration/signup.html', {'form': form})
