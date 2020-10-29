157dbd3c6482bb7386f98be79fb2b64b1c3b6dec
Ronny <mwithiamoki@gmail.com>
2020-10-30 01:10:05 +0300

    added class testproject and defined setup testinstance save delete and treardown on test

--- app/tests.py
+++ app/tests.py
@@ -17,3 +17,41 @@ test_project = Project(
     content='8',
     vote_submissions='87'
 )
+
+class TestProject(TestCase):
+    def setUp(self) -> None:
+        self.new_project = Project(
+            id=1,
+            title='Hello world',
+            details='a hello world app',
+            link='https://github.com/hello.git',
+            user=new_user,
+            image='/media/hello.jpg',
+            user_project_id='1',
+            design='7',
+            usability='8',
+            creativity='9',
+            content='8',
+            vote_submissions='87'
+
+        )
+
+    def testInstance(self):
+        self.assertTrue(isinstance(self.new_project, Project))
+
+    def testSaveProject(self):
+        before = Project.objects.all()
+        self.new_project.save()
+        after = Project.objects.all()
+
+        self.assertLess(before, after)
+
+    def testDeleteProject(self):
+        before = Project.objects.all()
+        self.new_project.delete()
+        after = Project.objects.all()
+
+        self.assertGreater(before, after)
+
+    def tearDown(self) -> None:
+        Project.objects.all().delete()
