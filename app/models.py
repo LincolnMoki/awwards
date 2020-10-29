479341bc8bc1d6d3f3c2e3d33705f41c85c712f3
Ronny <mwithiamoki@gmail.com>
2020-10-30 00:44:33 +0300

    created class project on models and defined

--- app/models.py
+++ app/models.py
@@ -31,3 +31,38 @@ class Profile(models.Model):
 
     class Meta:
         db_table = 'profiles'
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    link = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    image = CloudinaryField('image')
+    user_project_id = models.IntegerField(default=0)
+    design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    creativity = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    vote_submissions = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+        
+    @classmethod
+    def fetch_all_images(cls):
+        all_images = Project.objects.all()
+        return all_images
+
+    @classmethod
+    def get_single_project(cls, project):
+        project = cls.objects.get(id=project)
+        return project
+
+    @classmethod
+    def search_project_by_title(cls, search_term):
+        project = cls.objects.filter(title__icontains=search_term)
+        return project
+
+    class Meta:
+        db_table = 'projects'
+        ordering = ['-id']
